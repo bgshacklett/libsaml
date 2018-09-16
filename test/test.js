3cbae812c6eb176d3cfdae395e53970af9df4b9f
Brian Shacklett <brian@digital-traffic.net>
2018-09-15 20:44:16 -0400

    Fix linter errors in test.js

--- test/test.js
+++ test/test.js
@@ -1,91 +1,94 @@
+/* global beforeEach describe it */
+
 // Saml2js Tests
 // =============
 // The tests for this module.
 // jshint esnext: true
 
 const expect  = require('unexpected');
-const Saml2js = require('../index');
 const fs      = require('fs');
+const path    = require('path');
+const Saml2js = require('../index');
 
-var parser;
+let parser;
 
-beforeEach('pass SAML response to Saml2js', function(done) {
-  fs.readFile(__dirname + '/sample.saml', function(err, data) {
+beforeEach('pass SAML response to Saml2js', (done) => {
+  fs.readFile(path.join(__dirname, 'sample.saml'), (err, data) => {
     if (err) throw err;
     parser = new Saml2js(data);
     done();
   });
 });
 
-describe('Saml2js', function() {
-  describe('#parse()', function() {
-    it('should have a value', function() {
+describe('Saml2js', () => {
+  describe('#parse()', () => {
+    it('should have a value', () => {
       expect(parser.parsedSaml, 'to be defined');
     });
 
-    it('should return an object', function() {
+    it('should return an object', () => {
       expect(parser.parsedSaml, 'to be an', 'object');
     });
 
-    it('should have 10 elements in attributes', function() {
+    it('should have 10 elements in attributes', () => {
       expect(Object.keys(parser.parsedSaml.attributes), 'to have length', 10);
     });
   });
 
-  describe('#parse()', function() {
-    it('should parse base64 encoded SAML', function(done) {
-      fs.readFile(__dirname + '/base64.saml', {encoding: 'utf8'}, function(err, saml) {
+  describe('#parse()', () => {
+    it('should parse base64 encoded SAML', (done) => {
+      fs.readFile(path.join(__dirname, 'base64.saml'), { encoding: 'utf8' }, (err, saml) => {
         if (err) done(err);
-        var base64parser = new Saml2js(saml);
+        const base64parser = new Saml2js(saml);
         expect(base64parser.toObject(), 'to be an', 'object');
         done();
       });
     });
 
-    it('should contain the expected data', function(done) {
-      fs.readFile(__dirname + '/base64.saml', {encoding: 'utf8'}, function(err, saml) {
+    it('should contain the expected data', (done) => {
+      fs.readFile(path.join(__dirname, 'base64.saml'), { encoding: 'utf8' }, (err, saml) => {
         if (err) done(err);
-        var base64parser = new Saml2js(saml);
+        const base64parser = new Saml2js(saml);
         expect(base64parser.getAttribute('transfer type')[0], 'to be', 'Completed Application');
         done();
       });
     });
   });
 
-  describe('#toJSON()', function() {
-    it('should return a string', function() {
+  describe('#toJSON()', () => {
+    it('should return a string', () => {
       expect(parser.toJSON(), 'to be a', 'string');
     });
 
-    it('should be valid JSON', function() {
+    it('should be valid JSON', () => {
       expect(JSON.parse(parser.toJSON()), 'to be ok');
     });
   });
 
-  describe('#getAttribute()', function() {
-    it('should get attributes by name', function() {
+  describe('#getAttribute()', () => {
+    it('should get attributes by name', () => {
       expect(parser.getAttribute('transfer type')[0], 'to be', 'Completed Application');
     });
 
-    it('should get attributes case-insensitively', function() {
+    it('should get attributes case-insensitively', () => {
       expect(parser.getAttribute('FfE ASSIgned Consumer Id')[0], 'to be a', 'string');
     });
 
-    it('should return an empty string if the attribute is empty', function() {
+    it('should return an empty string if the attribute is empty', () => {
       expect(parser.getAttribute('Exception Reason')[0], 'to be empty');
     });
 
-    it('should return an empty array if the key does not exist', function() {
+    it('should return an empty array if the key does not exist', () => {
       expect(parser.getAttribute('some undefined key'), 'to be empty');
     });
   });
 
-  describe('#toObject()', function() {
-    it('should return an object', function() {
+  describe('#toObject()', () => {
+    it('should return an object', () => {
       expect(parser.toObject(), 'to be an', 'object');
     });
 
-    it('should have 10 elements in attributes', function() {
+    it('should have 10 elements in attributes', () => {
       expect(Object.keys(parser.toObject().attributes), 'to have length', 10);
     });
   });
